8bcedbe116adbd587064fc09c206f3c4ccbd6d39
DHINAKAR P <dhinakarintech@gmail.com>
2018-04-05 16:55:30 +0530

    Loading indicator
    Loading indicator

--- src/pages/tickets/ticketdetails/ticketdetails.ts
+++ src/pages/tickets/ticketdetails/ticketdetails.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { LoginPage } from '../../../pages/login/login';
 import { TicketCreationService } from '../../../shared';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-ticketdetails',
@@ -33,17 +34,23 @@ export class TicketDetailsPage {
     }
   };
 
-  constructor(public navParams: NavParams, public navCtrl: NavController, public ticketCreationService: TicketCreationService) {
+  constructor(public navParams: NavParams, public navCtrl: NavController,
+     public ticketCreationService: TicketCreationService,public loadingController:LoadingController) {
     console.log(this.navParams.get('id'));
     var id = this.navParams.get('id');
+    
     this.getTicketDetails(id);
   }
 
   getTicketDetails(id) {
+    let loading = this.loadingController.create({content : "Getting ticket details"});
+    loading.present();
     this.ticketCreationService.get_ticket_byID(id).subscribe((data) => {
       console.log("------>>> ", data);
+
       this.ticketDetails = data;
       console.log("--->> ", this.ticketDetails)
+      loading.dismissAll();
     });
   }
 
